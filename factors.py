cd270520ad06bbdeab836820523afe54945088d0
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-10-12 17:23:09 +0100

    commit

---
+++ factors.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+
+
+try:
+    file_name = sys.argv[1]
+except Exception:
+    raise TypeError("A file was not passed.")
+
+count = 0
+
+with open(file_name) as f:
+    line = f.readlines()
+    for i in line:
+        count += 1
+        lineNum = i.strip()
+        if isinstance(lineNum, int) and lineNum > 1:
+            try:
+                for i in range(2, lineNum + 1):
+                    result = lineNum % i
+                    if result == 0:
+                        print("{}={}*{}".format(lineNum, int(result), i))
+                        break
+            except Exception:
+                pass
+
+        else:
+            raise TypeError("Not an integer")
